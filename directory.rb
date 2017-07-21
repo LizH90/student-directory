38acb10d609ecbd6b6db68a2f48bfa46c97888d0
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-07-21 19:14:15 +0100

    10 refactoring

--- directory.rb
+++ directory.rb
@@ -1,38 +1,48 @@
 
+@students = []
+
 def interative_menu
-  students = []
+  @students = []
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit " # 9 because we'll be adding more items
+    print_menu
     # 2. read the input and save it into a variable
     selection = gets.chomp
     # 3. do what the user has asked
-    case selection
-    when "1"
-      # input the students
-      students = input_students
-    when "2"
-      # show the students
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you mean, try again"
-    end
+    process(selection)
   end
 end
 
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit " # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
 
 def input_students
   puts "Please enter the names of the students".center(30)
   puts "To finish, just hit return twice".center(30)
   #create an empty array
-  students =[]
+  @students = []
   # get the first names
   name = gets.gsub(/\n/,"")
 
@@ -44,49 +54,49 @@ def input_students
       cohort = :november
     end
     # add the student hash to the array with default cohort as November
-    students << {name: name, cohort: cohort}
+    @students << {name: name, cohort: cohort}
     #let use know student count take into account singular student case
-    if students.count == 1
-      puts "Now we have #{students.count} student".center(30)
+    if @students.count == 1
+      puts "Now we have #{@students.count} student".center(30)
     else
-      puts "Now we have #{students.count} students".center(30)
+      puts "Now we have #{@students.count} students".center(30)
     end
     #get another name from the user
     name = gets.gsub(/\n/,"")
 
   end
   # return array of students
-  students
+  @students
 end
 
-def print_header(students)
-  if students.count <= 0
+def print_header
+  if @students.count <= 0
     return
   end
   puts "The students of Villains Academy are".center(30)
   puts "-------------".center(30)
 end
 
-def print(students)
-  if students.count <= 0
+def print_student_list
+  if @students.count <= 0
     return
   end
   i = 0
-  while i < students.count
-  puts "#{students[i][:name]} (#{students[i][:cohort]})".center(30)
+  while i < @students.count
+  puts "#{@students[i][:name]} (#{@students[i][:cohort]})".center(30)
   i += 1
   end
 end
 
 # update to take into account number of students and give singular form
-def print_footer(students)
-  if students.count <= 0
+def print_footer
+  if @students.count <= 0
     return
   end
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student".center(30)
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student".center(30)
   else
-    puts "Overall, we have #{students.count} great students".center(30)
+    puts "Overall, we have #{@students.count} great students".center(30)
   end
 end
 
